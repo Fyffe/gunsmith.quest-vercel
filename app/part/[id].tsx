c2bdc6becf30a99de090c2087ccead50fff88721
Fyffe <13838188+Fyffe@users.noreply.github.com>
2025-09-22 12:29:19 +0200

    Removed gesture detector from part display

--- app/part/[id].tsx
+++ app/part/[id].tsx
@@ -15,7 +15,6 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import React, { useEffect, useRef, useState } from 'react';
 import { Animated, Dimensions, FlatList, Platform, Pressable, StyleSheet, Switch, View } from "react-native";
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Svg, { Defs, Path, Pattern, Rect } from 'react-native-svg';
 
 const { width, height } = Dimensions.get('window');
@@ -39,7 +38,7 @@ export default function Index() {
   const overlayOpacity = useRef(new Animated.Value(1)).current;
 
   const [weaponBuildReady, setWeaponBuildReady] = useState(false);
-  
+
   const toggleSwitch = () => {
     if (id) toggleCompleted(id.toString());
   };
@@ -111,139 +110,124 @@ export default function Index() {
     }
   };
 
-  const swipeGesture = Gesture.Pan()
-  .activeOffsetX([-50, 50]) // only react if horizontal movement > 50px
-  .failOffsetY([-50, 50])   // fail gesture if vertical movement > 50px
-  .onEnd((event) => {
-    if (!data.parts) return;
-
-    if (event.translationX < 0) {
-      handleNext()
-    } else if (event.translationX > 0) {
-      handlePrev()
-    }
-  }).runOnJS(true);
-
   return (
-    <View style={{ flex: 1 }} className='bg-[#1c1c1c]'>
-      {showOverlay && (
-        <Animated.View
-          style={{
-            ...StyleSheet.absoluteFillObject,
-            backgroundColor: '#1c1c1c',
-            justifyContent: 'center',
-            alignItems: 'center',
-            opacity: overlayOpacity,
-            zIndex: 999,
-          }}
-        >
-          <LoadingDots />
-        </Animated.View>
-      )}
+      <View style={{ flex: 1 }} className='bg-[#1c1c1c]'>
+        {showOverlay && (
+            <Animated.View
+                style={{
+                  ...StyleSheet.absoluteFillObject,
+                  backgroundColor: '#1c1c1c',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  opacity: overlayOpacity,
+                  zIndex: 999,
+                }}
+            >
+              <LoadingDots />
+            </Animated.View>
+        )}
 
-      {(fontsLoaded && !loading) && (
-        <FlatList
-          data={[]}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={null}
-          ListHeaderComponent={
-            <View>
-              <View className='flex flex-row justify-between px-4 py-8'>
-                <CustomButton
-                  title="Back"
-                  onPress={handlePrev}
-                  disabled={currentIndex === 0}
-                />
-                <CustomButton
-                  title="Next"
-                  onPress={handleNext}
-                  disabled={currentIndex === data.parts.length - 1}
-                />
-              </View>
-
-              <View className='flex flex-row items-center px-4 py-8'>
-                {Platform.OS === 'web' ? (
-                  <>
-                    <Pressable
-                      onPress={toggleSwitch}
-                      style={[
-                        styles.webSwitch,
-                        { backgroundColor: isCompleted ? '#767577' : '#767577' },
-                      ]}
-                    >
-                      <View
-                        style={[
-                          styles.thumb,
-                          {
-                            left: isCompleted ? 24 : 2,
-                            backgroundColor: isCompleted ? '#f4f3f4' : '#f4f3f4',
-                          },
-                        ]}
+        {(fontsLoaded && !loading) && (
+            <FlatList
+                data={[]}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={null}
+                ListHeaderComponent={
+                  <View>
+                    <View className='flex flex-row justify-between px-4 py-8'>
+                      <CustomButton
+                          title="Back"
+                          onPress={handlePrev}
+                          disabled={currentIndex === 0}
                       />
-                    </Pressable>
-                    <Pressable onPress={toggleSwitch}>
-                      <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
-                    </Pressable>
-                  </>
-                ) : (
+                      <CustomButton
+                          title="Next"
+                          onPress={handleNext}
+                          disabled={currentIndex === data.parts.length - 1}
+                      />
+                    </View>
 
-                  <>
-                    <Switch
-                      value={isCompleted}
-                      onValueChange={toggleSwitch}
-                      trackColor={{ false: '#767577', true: '#767577' }}
-                      thumbColor={isCompleted ? '#f4f3f4' : '#f4f3f4'}
-                      ios_backgroundColor="#3e3e3e"
-                    />
-                    <Pressable onPress={toggleSwitch}>
-                      <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
-                    </Pressable>
-                  </>
-                )}
-              </View>
+                    <View className='flex flex-row items-center px-4 py-8'>
+                      {Platform.OS === 'web' ? (
+                          <>
+                            <Pressable
+                                onPress={toggleSwitch}
+                                style={[
+                                  styles.webSwitch,
+                                  { backgroundColor: isCompleted ? '#767577' : '#767577' },
+                                ]}
+                            >
+                              <View
+                                  style={[
+                                    styles.thumb,
+                                    {
+                                      left: isCompleted ? 24 : 2,
+                                      backgroundColor: isCompleted ? '#f4f3f4' : '#f4f3f4',
+                                    },
+                                  ]}
+                              />
+                            </Pressable>
+                            <Pressable onPress={toggleSwitch}>
+                              <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
+                            </Pressable>
+                          </>
+                      ) : (
 
-              <GestureDetector gesture={swipeGesture}>
-                <View>
-                  <CustomTitleText className='text-4xl text-white md:text-center mb-6 px-4'>
-                    Part {id}
-                  </CustomTitleText>
+                          <>
+                            <Switch
+                                value={isCompleted}
+                                onValueChange={toggleSwitch}
+                                trackColor={{ false: '#767577', true: '#767577' }}
+                                thumbColor={isCompleted ? '#f4f3f4' : '#f4f3f4'}
+                                ios_backgroundColor="#3e3e3e"
+                            />
+                            <Pressable onPress={toggleSwitch}>
+                              <CustomText className='text-white text-xl ml-2'>{`Mark as ${isCompleted ? 'incomplete' : 'complete'}`}</CustomText>
+                            </Pressable>
+                          </>
+                      )}
+                    </View>
 
-                  <WeaponBuild partId={Number(id)} onReady={() => setWeaponBuildReady(true)} />
-                </View>
-              </GestureDetector>
+                    <View>
+                      <CustomTitleText className='text-4xl text-white md:text-center mb-6 px-4'>
+                        Part {id}
+                      </CustomTitleText>
 
-            </View>
-          }
-        />
-      )}
+                      <WeaponBuild partId={Number(id)} onReady={() => setWeaponBuildReady(true)} />
+                    </View>
 
-      <Svg width={width} height={height} style={{ position: 'absolute', zIndex: -1, opacity: 0.01 }}>
-        <Defs>
-          <Pattern
-            id="stripes"
-            patternUnits="userSpaceOnUse"
-            width="20"
-            height="20"
-            patternTransform="rotate(45)"
-          >
-            <Path d="M0 0 L20 0 L20 10 L0 10 Z" fill="#FFC0CB" />
-          </Pattern>
-        </Defs>
-        <Rect width="100%" height="100%" fill="url(#stripes)" />
-      </Svg>
+                  </View>
+                }
+            />
+        )}
 
-      <View style={styles.gradientWrapper} pointerEvents='none'>
-        <LinearGradient
-          colors={ isCompleted ? ['rgba(26, 123, 55, 0.35)', 'transparent'] : ['rgba(136, 74, 28, 0.35)', 'transparent']
-          }
-          start={{ x: 0.5, y: 1 }}
-          end={{ x: 0.5, y: 0 }}
-          locations={[0, 0.7]}
-          style={styles.gradientOverlay}
-        />
+        <Svg width={width} height={height} style={{ position: 'absolute', zIndex: -1, opacity: 0.01 }}>
+          <Defs>
+            <Pattern
+                id="stripes"
+                patternUnits="userSpaceOnUse"
+                width="20"
+                height="20"
+                patternTransform="rotate(45)"
+            >
+              <Path d="M0 0 L20 0 L20 10 L0 10 Z" fill="#FFC0CB" />
+            </Pattern>
+          </Defs>
+          <Rect width="100%" height="100%" fill="url(#stripes)" />
+        </Svg>
+
+        <View style={styles.gradientWrapper} pointerEvents='none'>
+          <LinearGradient
+              colors={ isCompleted ? ['rgba(26, 123, 55, 0.35)', 'transparent'] : ['rgba(136, 74, 28, 0.35)', 'transparent']
+              }
+              start={{ x: 0.5, y: 1 }}
+              end={{ x: 0.5, y: 0 }}
+              locations={[0, 0.7]}
+              style={styles.gradientOverlay}
+          />
+        </View>
+
       </View>
-
-    </View>
   );
 }
 
